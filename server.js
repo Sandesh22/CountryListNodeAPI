ab9a055334fca9eb4f6fa8acfc9652ae34f440e1
IN-ICE-LAP48\Sandesh <sandesh.karade@atpi.com>
2020-07-12 18:25:48 +0530

    added env file

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 // using body parser.
 app.use(bodyParser.urlencoded({ extended: false }));
-
+require("dotenv").config();
 app.use(cors());
 // custom imports.
 const router = require("./routes/index.router");
@@ -14,9 +15,10 @@ const router = require("./routes/index.router");
 app.use(router);
 
 // setting port number
-process.env.PORT = 52898;
+//process.env.PORT = 52898;
 
 // server listening.
+console.log(process.env.PORT);
 app.listen(process.env.PORT, () => {
   console.log(`node started at ${process.env.PORT}`);
 });
